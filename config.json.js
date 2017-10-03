8ad30205ab4729a5c475c4a74d14ee54a49c2cef
clangcn <clangcn@gmail.com>
2017-10-03 14:26:37 +0800

    serverchan 0.1.7

--- config.json.js
+++ config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "从路由器推送状态及通知的工具。", 
     "home_url": "Module_serverchan.asp", 
-    "md5": "751ac25a189799fdc122a5a00031b916", 
+    "md5": "f9915664c3cf76c968ca495d132ae555", 
     "module": "serverchan", 
     "title": "serverChan微信推送", 
     "version": "0.1.7"
